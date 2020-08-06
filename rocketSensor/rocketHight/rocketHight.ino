ee5e717cb52c42652d5ee9a8e6bba02b97192b1c
Baumgarten <Matthias.Baumgarten@otto.de>
2020-08-06 11:26:16 +0200

    lauffÃhiger zustand inklusive abort condition, jedoch noch mit serial ausgabe. getestet auf uno, nÃchster schritt ist nano test

--- rocketSensor/rocketHight/rocketHight.ino
+++ rocketSensor/rocketHight/rocketHight.ino
@@ -23,6 +23,7 @@ File myFile;
 int SSPin = 10;
 unsigned long millisMessbeginn; 
 String fileName = "rocket.csv";
+boolean counterToggle = true;
 
 // For BMP
 Adafruit_BMP280 bmp; // I2C
@@ -38,7 +39,7 @@ void SDInit(){
           //Serial.println("initialization failed!");
           statusSum+=1;
           }
-}
+}// END SDInit
 
 int FileInit(){
      
@@ -47,45 +48,66 @@ int FileInit(){
         //Serial.println("File existed - deleted File");
      }
      
+
+     // Init of file as begin of measurement
+     millisMessbeginn = millis();
+     
      myFile = SD.open(fileName, FILE_WRITE);
+       
     // if the file opened okay, write to it:
-    if (!myFile) {
-        //Serial.println("File opening successful");
+    if (!myFile) {   //File opening failed
         statusSum+=10;
     }
-}
+}// END FileInit
 
 void SDwrite(){
 
-         // Zeit seit arduino start;sdWrite simbolisiert Messbeginn
-        millisMessbeginn = millis();
+      
+
+
+        //Header nur einmal schreiben
+        if (counterToggle){ 
+          myFile.println("Sek/10_SinceWrite;TempInC;Pressure;AltitudeInM");
+          counterToggle = !counterToggle;               
+          Serial.print("begin writing on SD Card ...");//NOT NEEDED IN PRODUCTION
+        }
         
-        Serial.print("begin writing on SD Card ...");
-        myFile.println("SekSinceWrite;TempInC;Pressure;AltitudeInM");
-       
-        myFile.print((float)((millis()-millisMessbeginn)/1000));myFile.print(";");
+        
+        Serial.print((millis()-millisMessbeginn)/100);Serial.print(";");
+        Serial.print(temperature);Serial.print(";");
+        Serial.print(pressure);Serial.print(";");
+        Serial.println(altimeter);
+        
+    
+        myFile.print((millis()-millisMessbeginn)/100);myFile.print(";");
         myFile.print(temperature);myFile.print(";");
         myFile.print(pressure);myFile.print(";");
         myFile.println(altimeter);
         delay(300);     
-}
+}// END SDwrite
 
 // FOR BMP
 void BMPInit(){
     if (!bmp.begin(0x76)) {
-          Serial.println("BMP initialization failed!");
+          Serial.println("BMP initialization failed!");//NOT NEEDED IN PRODUCTION
           statusSum+=100;
           }
-}
+}// END BMPInit
+
 void BMPReadValues(){
-    //Read values from the sensor:
+  //Read values from the sensor:
   pressure = bmp.readPressure();
   temperature = bmp.readTemperature();
   altimeter = bmp.readAltitude (1013.2); //1013.2 Standard baromterischer Druck. Change the to your city current barrometric pressure
-}
+}// END BMPReadValues
+
+
+
 //############ AUTORUN FUNCTIONS #######
 void setup() {
-  Serial.begin(9600);
+
+  //NOT NEEDED IN P
+  Serial.begin(9600);//NOT NEEDED IN PRODUCTION
   
   //BMP Part
   BMPInit();
@@ -94,38 +116,32 @@ void setup() {
   SDInit(); //initialize SD
   FileInit();
 
-  //Status
+  //Status Part
   pinMode(StatusGreenPin, OUTPUT);
   pinMode(StatusRedPin, OUTPUT);
-   
-    
   if(statusSum==0){
     digitalWrite(StatusGreenPin, HIGH);
   }else{
     digitalWrite(StatusRedPin, HIGH);
   }
      
-}
+}// END setup
 
 void loop() {
-  // ...
-  //Wenn kein Fehlerstatus
-  Serial.print("StatusCode:");
-  Serial.println(statusSum);
-  if(statusSum=0){
-     BMPReadValues();
-     SDwrite();     
-  }else {
-    myFile.close();
-  }
-
-
-//ABORTKONDITION
-  // Nach 2 min File Close
-  if (millis()>120000){
-    myFile.close();
-    digitalWrite(StatusGreenPin, HIGH);
-    digitalWrite(StatusRedPin, HIGH);
-  }
   
-}
+  Serial.print("StatusCode:");//NOT NEEDED IN PRODUCTION
+  Serial.println(statusSum);  //NOT NEEDED IN PRODUCTION
+
+  if(statusSum==0){   //Wenn kein Fehlerstatus
+  
+    if (millis()<60000){ // Nach 2 min File Close 120000
+      BMPReadValues();
+      SDwrite();
+    }else{
+          Serial.println("END OF RUN - CLOSING FILE");  //NOT NEEDED IN PRODUCTION
+          myFile.close();
+          digitalWrite(StatusGreenPin, HIGH);
+          digitalWrite(StatusRedPin, HIGH);
+    }
+  }
+}// END loop
